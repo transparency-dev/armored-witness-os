d220e971305fb2fbd45f931128c5292e182d41c2
Andrea Barisani <andrea@inversepath.com>
2023-07-30 15:48:12 +0200

    workaround fidati pkg bug on consecutive commands (#39)

--- cmd/witnessctl/api.go
+++ cmd/witnessctl/api.go
@@ -95,10 +95,10 @@ func ota(taELFPath string, taSigPath string) (err error) {
 		return errors.New("trusted applet signature path must be specified (-O)")
 	}
 
-	s, err := status()
-
-	if err != nil {
-		return
+	// TODO: replace with status() after fixing
+	// https://github.com/gsora/fidati/issues/5
+	if err := detect(); err != nil {
+		return err
 	}
 
 	taELF, err := os.ReadFile(taELFPath)
@@ -129,7 +129,8 @@ func ota(taELFPath string, taSigPath string) (err error) {
 		return errors.New("signature size exceeds maximum update chunk size")
 	}
 
-	log.Printf("sending trusted applet signature to armored witness %s", s.Serial)
+	log.Printf("sending trusted applet signature to armored witness")
+
 	if err = sendUpdateChunk(taSig, seq, total); err != nil {
 		return
 	}
@@ -145,7 +146,7 @@ func ota(taELFPath string, taSigPath string) (err error) {
 	}(start)
 	defer bar.Finish()
 
-	log.Printf("sending trusted applet payload to armored witness %x", s.Serial)
+	log.Printf("sending trusted applet payload to armored witness")
 
 	for i := 0; i < totalSize; i += chunkSize {
 		seq += 1
@@ -194,17 +195,13 @@ func cfg(dhcp bool, ip string, mask string, gw string, dns string, ntp string) e
 		NTPServer: ntp,
 	}
 
+	// TODO: replace with status() after fixing
+	// https://github.com/gsora/fidati/issues/5
 	if err := detect(); err != nil {
 		return err
 	}
 
-	s, err := status()
-
-	if err != nil {
-		return err
-	}
-
-	log.Printf("sending configuration update to armored witness %s", s.Serial)
+	log.Printf("sending configuration update to armored witness")
 
 	buf, err := conf.dev.Command(api.U2FHID_ARMORY_CFG, c.Bytes())
 
