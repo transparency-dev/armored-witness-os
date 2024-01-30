ad2ae61285a62493d78d9bc38cafb48ec2990929
Jay <jayhou@google.com>
2024-01-30 11:28:34 +0000

    Add identity counter to witness status data structures.

--- api/rpc/rpc.go
+++ api/rpc/rpc.go
@@ -49,6 +49,9 @@ type WitnessStatus struct {
 	Identity string
 	// IP is the currently-assigned IP address of the witness applet.
 	IP string
+	// IdentityCounter is incremented when the device is recovered and the device
+	// needs a new witness identity.
+	IdentityCounter uint32
 }
 
 // FirmwareUpdate represents a firmware update.
